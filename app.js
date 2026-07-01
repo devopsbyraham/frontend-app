bd5b38e0d461763fa65bc1c9283d7a443c24aa40
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2026-07-01 22:46:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
- res.send("Hello from Jenkins-ArgoCD Demo")
+ res.send("Hello from Jenkins-ArgoCD Demo-2")
 })
 
 app.listen(3000)
